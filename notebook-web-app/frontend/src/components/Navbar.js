1473781fee4394b1425574858358133e1e5c6cd3
Siva Koteswara Rao <sivapalepu01@gmail.com>
2026-05-05 09:51:01 +0000

    updated

--- notebook-web-app/frontend/src/components/Navbar.js
+++ notebook-web-app/frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 
 function Navbar(props) {
@@ -13,16 +14,16 @@ function Navbar(props) {
         navigate("/login");
     }
     
+    const [open,setOpen] = useState(false);
   return (
     <div>
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark fixed-top">
             <div className="container-fluid">
                 <h2 className="navbar-brand" style={{paddingBottom:"0px"}}>NoteBook</h2>
-                {/* <h2 className='navbar-brand'>UserNotes</h2> */}
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" onClick={()=>{setOpen(!open)}}>
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className={`collapse navbar-collapse ${open?"show":""}`} id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
